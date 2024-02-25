2c5b3afd088b35f82abc13121a90609c52d4d001
Crystal Hess <hesscrystal@cityuniversity.edu>
2024-02-25 10:30:40 -0800

    started initial descriptors

---
+++ AboutTheDataset.r
@@ -0,0 +1,13 @@
+library(readr)
+
+college_df <- read.csv("Community_Colleges_-_Budget__Revenue__Expenditure_-_Detail_Data.csv")
+# print(head(college_df, n = 10))
+
+cat("Num Variables = ", ncol(college_df))
+print(colnames(college_df))
+print(" ")
+
+cat("Num Observations = ", nrow(college_df))
+colleges <- sort(unique(college_df$Institution))
+cat("colleges = ", colleges)
+print(len(colleges))
